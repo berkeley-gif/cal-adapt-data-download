ba8ec636e1f1f2721ec2ef8dab11d9dc0b3c5896
melijimenez11 <melijimenez@berkeley.edu>
2024-08-26 18:20:10 +0100

    added color legend

--- app/components/Heatmap/Rendererer.tsx
+++ app/components/Heatmap/Rendererer.tsx
@@ -11,10 +11,11 @@ type RendererProps = {
     width: number;
     height: number;
     data: any;
-    setHoveredCell: (hoveredCell: InteractionData | null) => void
+    setHoveredCell: (hoveredCell: InteractionData | null) => void;
+    colorScale: d3.ScaleSequential<string>;
 }
 
-export default function Renderer({ width, height, data, setHoveredCell }: RendererProps) {
+export default function Renderer({ width, height, data, setHoveredCell, colorScale }: RendererProps) {
     // bounds = area inside the axis
     const boundsWidth = width - MARGIN.right - MARGIN.left
     const boundsHeight = height - MARGIN.top - MARGIN.bottom
@@ -28,7 +29,7 @@ export default function Renderer({ width, height, data, setHoveredCell }: Render
     const allYGroups = useMemo(() => data.coords.month.data, [data])
 
     // values for each square
-    const flatData: number[] = data.data.flat()
+    //const flatData: number[] = data.data.flat()
 
     // generate flat heatmap data
     const heatmapData = allYGroups.flatMap((month: number, yIdx: number) =>
@@ -54,13 +55,6 @@ export default function Renderer({ width, height, data, setHoveredCell }: Render
             .padding(0.01)
     }, [boundsHeight, allYGroups])
 
-    const min = d3.min(flatData)
-    const max = d3.max(flatData)
-
-    const colorScale = d3.scaleSequential<number>()
-        
-        .interpolator(d3.interpolateRgbBasis(["#FD6A55", "#EEE8DA", "#DAEAEE"]))
-        .domain([min, max])
 
     // Create x-axis labels (for years)
     const xLabels = allXGroups.map((year, i) => {
@@ -124,108 +118,6 @@ export default function Renderer({ width, height, data, setHoveredCell }: Render
         )
     })
 
-    /*  // groups
-    // list of unique items that will appear on the heatmap Y axis 
-    const allYGroups = useMemo(() => [...new Set(data.data.map((d) => d.y))], [data])
-    const allXGroups = useMemo(() => [...new Set(data.data.map((d) => d.x))], [data])
-
-    // x and y scales
-    const xScale = useMemo(() => {
-        return d3
-            .scaleBand()
-            .range([0, boundsWidth])
-            .domain(allXGroups)
-            .padding(0.01);
-    }, [data, width]);
-
-    const yScale = useMemo(() => {
-        return d3
-            .scaleBand()
-            .range([boundsHeight, 0])
-            .domain(allYGroups)
-            .padding(0.01);
-    }, [data, height]);
-
-    const filteredValues = data.map((d) => d.value).filter((value): value is number => value !== null)
-    const [min, max] = d3.extent(filteredValues)
-
-    if (!min || !max) {
-        return null
-    }
-
-    const colorScale = d3
-        .scaleSequential()
-        .interpolator(d3.interpolateRgb('#FD6A55', '#EEE8DA'))
-        .domain([min, max])
-
-    const allShapes = data.map((d, i) => {
-        const x = xScale(d.x)
-        const y = yScale(d.y)
-
-        if (d.value == null) {
-            return
-        }
-        return (
-            <rect
-                key={i}
-                r={4}
-                x={xScale(d.x)}
-                y={yScale(d.y)}
-                width={xScale.bandwidth()}
-                height={yScale.bandwidth()}
-                opacity={1}
-                fill={colorScale(d.value)}
-                rx={0}
-                stroke={"white"}
-                onMouseEnter={(e) => {
-                    setHoveredCell({
-                        xLabel: 'group ' + d.x,
-                        yLabel: 'group ' + d.y,
-                        xPos: x + xScale.bandwidth() + MARGIN.left,
-                        yPos: y + xScale.bandwidth() / 2 + MARGIN.top,
-                        value: Math.round(d.value * 100) / 100,
-                    })
-                }}
-                onMouseLeave={() => setHoveredCell(null)}
-                cursor="pointer"
-            />
-        )
-    })
-
-    const xLabels = allXGroups.map((name, i) => {
-        const xPos = xScale(name) ?? 0
-
-        return (
-            <text
-                key={i}
-                x={xPos + xScale.bandwidth() / 2}
-                y={boundsHeight + 10}
-                textAnchor="middle"
-                dominantBaseline="middle"
-            >
-                {name}
-            </text>
-
-        )
-    })
-
-    const yLabels = allYGroups.map((name, i) => {
-        const yPos = yScale(name) ?? 0
-
-        return (
-            <text
-                key={i}
-                x={-5}
-                y={yPos + yScale.bandwidth() / 2}
-                textAnchor="end"
-                dominantBaseline="end"
-            >
-                {name}
-            </text>
-
-        )
-    }) */
-
     useEffect(() => {
         console.log('groups')
 
@@ -234,7 +126,6 @@ export default function Renderer({ width, height, data, setHoveredCell }: Render
 
         console.log('allygroups')
         console.log(allYGroups)
-        console.log(flatData)
 
         console.log('heatmapdata')
         console.log(heatmapData)
