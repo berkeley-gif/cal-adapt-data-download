20e0fb50e5914893498542e09ff637717cd9f9a9
jfantauzza <jfantauzza@berkeley.edu>
2025-01-09 10:19:59 -0800

    feature/data explorer full-screen map

--- app/components/Data-Explorer/DataExplorer.tsx
+++ app/components/Data-Explorer/DataExplorer.tsx
@@ -5,7 +5,8 @@ import React, { useState, useEffect, useRef } from 'react'
 import { globalWarmingLevelsList } from '@/app/lib/data-explorer/global-warming-levels'
 import { metricsList } from '@/app/lib/data-explorer/metrics'
 import Map from './Map'
-
+import Grid from '@mui/material/Unstable_Grid2'
+import Typography from '@mui/material/Typography'
 type DataExplorerProps = {
     data: any
 }
@@ -15,9 +16,9 @@ export default function DataExplorer({ data }: DataExplorerProps) {
     const [metricSelected, setMetricSelected] = useState<number>(0)
 
     return (
-        <div>
-            Data Explorer content
+        <Grid container sx={{ height: '100%', flexDirection: "column", flexWrap: "nowrap", flexGrow: 1 }}>
+            <Typography variant="body1">Data Explorer content</Typography>
             <Map gwlSelected={gwlSelected} setGwlSelected={setGwlSelected} metricSelected={metricSelected} setMetricSelected={setMetricSelected} data={data}></Map>
-        </div>
+        </Grid>
     )
 }
--- app/components/Data-Explorer/Map.tsx
+++ app/components/Data-Explorer/Map.tsx
@@ -1,8 +1,11 @@
 'use client'
 
-import React, { useState, useEffect, useRef, useMemo } from 'react'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import React, { useState, useEffect, useRef, useMemo, forwardRef, useImperativeHandle } from 'react'
+import { Marker, Map, Layer, Source, MapMouseEvent, NavigationControl, MapRef, ScaleControl } from 'react-map-gl'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Unstable_Grid2'
 
-// The map props are inherited from the parent DataExplorer and can be used to set and access the Data Explorer's variables
 type MapProps = {
     metricSelected: number;
     gwlSelected: number;
@@ -11,13 +14,54 @@ type MapProps = {
     setGwlSelected: (gwl: number) => void,
 }
 
-export default function Map({ metricSelected, gwlSelected, data }: MapProps) {
+const MapboxMap = forwardRef<MapRef | null, MapProps>(
+    ({ metricSelected, gwlSelected, data, setMetricSelected, setGwlSelected }, ref) => {
+        const mapRef = useRef<MapRef | null>(null)
+        const [mapLoaded, setMapLoaded] = useState(false)
+
+        const initialViewState = {
+            longitude: -122.4,
+            latitude: 37.8,
+            zoom: 8
+        }
+
+        // Forward the internal ref to the parent using useImperativeHandle
+        useImperativeHandle(ref, () => mapRef.current as MapRef)
+
+        const handleMapLoad = (e: any) => {
+            const map = e.target;
+            mapRef.current = map;
+            setMapLoaded(true);
+        }
+
     return (
-        <div>
-            <p>{metricSelected}</p>
-            <p>{gwlSelected}</p>
-            Map Code
-        </div>
+        <Grid container sx={{ height: '100%', flexDirection: "column", flexWrap: "nowrap", flexGrow: 1 }}>
+            <Box>
+                <p>{metricSelected}</p>
+                <p>{gwlSelected}</p>
+            </Box>
+                <Box sx={{ height: '100%', flexDirection: "column", flexWrap: "nowrap", flexGrow: 1, position: "relative" }} id="map">
+                    <Map
+                        ref={mapRef}
+                        onLoad={handleMapLoad}
+                        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+                        initialViewState={initialViewState}
+                        mapStyle="mapbox://styles/mapbox/streets-v9"
+                        scrollZoom={false}
+                        minZoom={3.5}
+                        style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+                    >
+                        <NavigationControl position="bottom-left" />
+                        <ScaleControl position="bottom-right" maxWidth={100} unit="metric" />
+                    </Map>
+                </Box>
+        </Grid>
     )
-}
+    }
+)
+
+MapboxMap.displayName = 'MapboxMap'
+
+export default MapboxMap
+
 
