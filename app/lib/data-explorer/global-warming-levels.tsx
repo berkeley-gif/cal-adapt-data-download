4861d6bc646d3fc096fa7173c8036e78f6040d22
jfantauzza <jfantauzza@berkeley.edu>
2025-01-20 11:49:51 -0800

    fix/changed global warming levels to match the dataset

--- app/lib/data-explorer/global-warming-levels.tsx
+++ app/lib/data-explorer/global-warming-levels.tsx
@@ -1,22 +1,18 @@
 export const globalWarmingLevelsList = [
     {
         "id": 0,
-        "title": 'Current State 1.2°'
-    },
-    {
-        "id": 1,
         "title": '1.5°'
     },
     {
-        "id": 2,
+        "id": 1,
         "title": '2.0°'
     },
     {
-        "id": 3,
+        "id": 2,
         "title": '2.5°'
     },
     {
-        "id": 4,
+        "id": 3,
         "title": '3.0°'
     },
 ]
